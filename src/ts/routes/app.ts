786ad6e30a5d1b6f2a498738626d918681051efd
Mart van Weeghel <martvanweeghel@icloud.com>
2022-09-06 09:04:52 +0200

    Base URL

--- src/ts/routes/app.ts
+++ src/ts/routes/app.ts
@@ -1,6 +1,6 @@
 
 // URL for the API
-export const baseURL = 'https://api.stroom.martvanweeghel.nl'; //https://api.stroom.martvanweeghel.nl
+export const baseURL = 'https://api.stoom.martvanweeghel.nl'; //https://api.stoom.martvanweeghel.nl
 
 // Routes for the app
 export const appPath = {
